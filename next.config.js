f9135ffde18833954fa1aa37db33c308ac7c0837
claudiapeiovich14-ops <claudiapeiovich14@gmail.com>
2026-07-10 13:42:39 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
+const repoName = "Cocina-Sin-Estres-mai";
+const isGithubPages = process.env.GITHUB_ACTIONS === "true";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
+  basePath: isGithubPages ? `/${repoName}` : "",
+  assetPrefix: isGithubPages ? `/${repoName}/` : "",
+  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
